2a351a60bdda59140e1f3e7fd86c71238fd8712a
student <yurashmeks@gmail.com>
2023-11-30 17:28:48 +0200

    Заповнили інформацію про студентів

--- lesson2.py
+++ lesson2.py
@@ -1,7 +1,13 @@
 class Group_1012:
-    pass
-student1 = Group_1012
-student2 = Group_1012
-student3 = Group_1012
-student4 = Group_1012
-student5 = Group_1012
+    def __int__(self, name, gen, age, avarage_makr):#ініціалізація атрибутів класу
+        self.name = name
+        self.gen = gen
+        self.age = age
+        self.avarage_makr = avarage_makr
+
+
+student1 = Group_1012("Гончарук Олександр", "Чол.", 12, 12)#Об'экт, тобто екземпляр класу
+student2 = Group_1012("Трофимишин Владислав", "чол", 12, 12)
+student3 = Group_1012("Радомська Дарина",  "жін.",  13,  12)
+student4 = Group_1012("Коваленко Макар", "чол", 13, 11.1)
+student5 = Group_1012("Юраш Максим", "Чол.", 14, 10)
